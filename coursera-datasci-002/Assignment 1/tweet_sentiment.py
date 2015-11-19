332cde64a3f5398c38889b5b6d434336247122f6
Brienna Herold <brienna.herold@gmail.com>
2015-11-18 23:08:56 -0500

    Completed Problems 1 and 2

---
+++ coursera-datasci-002/Assignment 1/tweet_sentiment.py
@@ -0,0 +1,33 @@
+import sys
+import json
+
+
+def analyzeSentiment(tweet, scores):
+	words = tweet.split()  # splits tweet into words
+	total = 0
+	for word in words:  # for word in tweet
+		if word in scores:  # if word is a sentiment term
+			total += scores[word]  # add sentiment score to total
+	print total
+
+
+def main():
+	# creates dictionary of terms and sentiment scores
+    afinnfile = open("AFINN-111.txt")
+    scores = {}
+    for line in afinnfile:
+    	term, score = line.split("\t")  # cuz the file is tab-delimited
+    	scores[term] = int(score)  # assigns score to term
+
+    # parses tweets
+    tweetFile = open(sys.argv[1])
+    for line in tweetFile:
+    	parsedJSON = json.loads(line)  # parses line into, for this, a dictionary
+    	if 'text' in parsedJSON:  # if dictionary contains text as key
+    		analyzeSentiment(parsedJSON['text'], scores)  # derive text's sentiment
+    	else:  # if dictionary doesn't contain text field
+    		print 0  # tweet contains no sentiment
+
+
+if __name__ == '__main__':
+    main()
